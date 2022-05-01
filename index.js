8367d296745476f207bba1f0e9b1367478762096
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-05-01 15:07:15 -0400

    commit tree 8dc7107b537bf5a56ed67ea1dc08d63397727baf

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let multiply;
+const num1= 15.5;
+const num2=4;
+multiply = num1*num2;
+let random = Math.floor(Math.random()* 100) +1 ;
+let num3 =16;
+let num4 = 4;
+let mod = num3/num4;
+const x=(12,14,15,17,20)
+let max=Math.max(x);
